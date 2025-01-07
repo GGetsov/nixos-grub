95b84dfa179d42873da4b4139807a89c2bf6f0fa
GGetsov <g.getsov.dev@gmail.com>
2025-01-07 21:26:07 +0200

    test 8

--- default.nix
+++ default.nix
@@ -5,15 +5,17 @@
 let
   version = "1.0.0";
 in
-  # boot.loader.grub.splashImage = src/splash_image.png;
-  stdenvNoCC.mkDerivation {
-    pname = "nixos-grub";
-    inherit version;
-    src = ./src;
-    installPhase = "cp -r . $out";
-    meta = {
-      description = "IDK a theme i modified to match catppuccin macchiato color scheme";
-      license = lib.licenses.gpl3Only;
-      platforms = lib.platforms.linux;
+  boot.loader.grub = {
+    splashImage = src/splash_image.png;
+    theme = stdenvNoCC.mkDerivation {
+      pname = "nixos-grub";
+      inherit version;
+      src = ./src;
+      installPhase = "cp -r . $out";
+      meta = {
+        description = "IDK a theme i modified to match catppuccin macchiato color scheme";
+        license = lib.licenses.gpl3Only;
+        platforms = lib.platforms.linux;
+      };
     };
   }
