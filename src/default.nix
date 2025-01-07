81612304f2677feee2b590321918077bb0aba5e6
GGetsov <g.getsov.dev@gmail.com>
2025-01-07 20:59:53 +0200

    test 4

---
+++ src/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  stdenvNoCC,
+}:
+let
+  version = "1.0.0";
+in
+  boot.loader.grub.splashImage = src/splash_image.png;
+  stdenvNoCC.mkDerivation {
+    pname = "nixos-grub";
+    inherit version;
+    src = ./src/;
+    installPhase = "cp -r . $out";
+    meta = {
+      description = "IDK a theme i modified to match catppuccin macchiato color scheme";
+      license = lib.licenses.gpl3Only;
+      platforms = lib.platforms.linux;
+    };
+  }
